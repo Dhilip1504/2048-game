cf061d8934fff0fd9c521ba6b3abf41512977b2e
Dhilip kumar <dhilip1504@gmail.com>
2022-02-05 14:46:49 +0530

    display update

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-board = [[0, 0, 0, 0], [0, 2, 0, 0], [0, 0, 0, 4], [0, 0, 0, 0]]
+board = [[0, 16, 0, 0], [0, 2, 0, 0], [0, 0, 0, 4], [0, 0, 0, 0]]
 
 
 def display():
+
+    largest = board[0][0]
+    for row in board:
+        for element in row:
+            if element > largest:
+                largest = element
+
+    numSpaces = len(str(largest))
+
     for row in board:
         currRow = '|'
         for element in row:
             if element == 0:
-                currRow += ' |'
+                currRow += " " * numSpaces + '|'
             else:
-                currRow += str(element) + '|'
+                currRow += " " * (numSpaces - len(str(element))
+                                  ) + str(element) + '|'
 
         print(currRow)
     print()
